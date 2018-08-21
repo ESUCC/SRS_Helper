1b292b8e46255f4da8c78ccab42ab6987ed9d45a
Ariel@elementary-rog-20aj <ariel.kruger@gmail.com>
2018-08-21 09:02:17 -0300

    fixing names

--- src/SRS/Forms/Factory/View/Helper/FormRendederFactory.php
+++ src/SRS/Forms/Factory/View/Helper/FormRendederFactory.php
@@ -11,6 +11,7 @@ class FormRendererFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $renderer = $container->get('renderer.bootstrap');
+        $renderer->setGlobalFormConfig($container->get('config')['srs_form']);
         $renderer->init();
         
         return new FormRenderer($renderer);
